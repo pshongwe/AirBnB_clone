75ac69a06c2254bf91fb01e28ef5045fe6d4fd9e
Darknught <hassino66@yahoo.com>
2024-01-09 15:14:30 +0000

    code

--- models/base_model.py
+++ models/base_model.py
@@ -7,24 +7,43 @@ from datetime import datetime
 
 class BaseModel:
     """A Base class with all public instances"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization of the class"""
-        self.id = str(uuid.uuid4())
 
-        current_time = datetime.utcnow()
-        self.created_at = current_time
-        self.updated_at = current_time
+        # Check if kwargs is not empty
+        if kwargs:
+            # Iterate through the key-value pairs in kwargs
+            for key, value in kwargs.items():
+                # Exclude the __class__ key
+                if key != '__class__':
+                    setattr(self, key, value)
+
+                    # Convert created_at and updatated_at strs to datetime objs
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f'))
+
+        else:
+            # Generate a unique ID for the instance
+            self.id = str(uuid.uuid4())
+
+            # Set the created_at and updated_at attrbs to the current datetime
+            current_time = datetime.utcnow()
+            self.created_at = current_time
+            self.updated_at = current_time
 
     def save(self):
         """Update the updated_at attribute to the current datetime"""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-        """Creates a dictionary containing only instance att that have been set"""
+        """Converts created_at and updated_at to str objects in ISO format"""
         c_at_string = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         u_at_string = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
 
-        my_dict = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
+        my_dict = {
+                k: v for k, v in self.__dict__.items() if not k.startswith('_')
+                }
 
         my_dict['created_at'] = c_at_string
         my_dict['updated_at'] = u_at_string
