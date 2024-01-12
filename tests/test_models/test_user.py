aa4cc2d12fd9ec75b8fead581036e10a951e45b1
Pibo Shongwe <piboman@yahoo.com>
2024-01-12 12:26:15 +0200

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -13,25 +13,30 @@ from models.base_model import BaseModel
 class TestUser(unittest.TestCase):
     """ User class unit tests class """
     def setUp(self):
+        """Setup"""
         self.user = User()
         self.models_storage = storage.all()
 
     def tearDown(self):
+        """Teardown"""
         storage.delete()
 
     def test_user_attributes(self):
+        """Test user attributes"""
         self.assertTrue(hasattr(self.user, 'email'))
         self.assertTrue(hasattr(self.user, '_password'))
         self.assertTrue(hasattr(self.user, 'first_name'))
         self.assertTrue(hasattr(self.user, 'last_name'))
 
     def test_password_hashing(self):
+        """Test password hashing"""
         password = "password123"
         self.user.password = password
         hashed_password = hashlib.md5(password.encode()).hexdigest()
         self.assertEqual(self.user._password, hashed_password)
 
     def test_user_creation(self):
+        """Test user create"""
         self.assertIsInstance(self.user, User)
         self.assertTrue(self.user.id)
         self.assertTrue(self.user.created_at)
