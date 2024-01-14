7abe22dbc1285f94e8c26e622ca4e3c797317075
Pibo Shongwe <piboman@yahoo.com>
2024-01-15 00:57:16 +0200

    Update console.py

--- console.py
+++ console.py
@@ -34,10 +34,10 @@ class HBNBCommand(cmd.Cmd):
         checks = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
         if checks:
             class_name, command, args = checks.groups()
+            args_checks = re.search(r"^\"([^\"]*)\"(?:, (.*))?$", args)
             if args_checks:
                 instance_id, attribute_part = args_checks.groups()
             attr_pt = f"{command} {class_name} {instance_id} {attribute_part}"
-            args_checks = re.search(r"^\"([^\"]*)\"(?:, (.*))?$", args)
             if args is None:
                 line = f"{command} {class_name}"
             else:
