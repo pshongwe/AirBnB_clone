72e64c3ad0c49e121f1b939923de10cbe4bb5bf1
Pibo Shongwe <piboman@yahoo.com>
2024-01-15 09:46:40 -0800

    u

--- models/user.py
+++ models/user.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
 """ class User """
 import models
-import hashlib
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """ class representation of User object """
     email = ""
-    _password = ""
+    password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initialize user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """ hash password """
-        self._password = hashlib.md5(pwd.encode()).hexdigest()
